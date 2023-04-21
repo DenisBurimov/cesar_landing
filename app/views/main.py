ede6dab93e7fa316461c2006aaffdcda5f8f7f3b
denys <denysburimov@gmai.com>
2023-04-21 16:31:41 +0300

    form fix

--- app/views/main.py
+++ app/views/main.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, flash
+from flask import Blueprint, render_template, flash, redirect, url_for
 from app.forms import ContactForm
 from app.logger import log
 
@@ -11,4 +11,5 @@ def index():
     if form.validate_on_submit():
         log(log.INFO, "Form submitted")
         flash("Submitted", "info")
+        return redirect(url_for("main.index"))
     return render_template("index.html", form=form)
