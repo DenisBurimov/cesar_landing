5dcc05ab7ddf7e38d8b6da2074eece686762b476
denys <denysburimov@gmai.com>
2023-04-21 16:26:13 +0300

    form submit

--- app/views/main.py
+++ app/views/main.py
@@ -1,12 +1,14 @@
 from flask import Blueprint, render_template, flash
 from app.forms import ContactForm
+from app.logger import log
 
 main_blueprint = Blueprint("main", __name__)
 
 
-@main_blueprint.route("/")
+@main_blueprint.route("/", methods=["GET", "POST"])
 def index():
     form = ContactForm()
     if form.validate_on_submit():
+        log(log.INFO, "Form submitted")
         flash("Submitted", "info")
     return render_template("index.html", form=form)
