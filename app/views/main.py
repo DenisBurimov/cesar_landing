3cc8bbcca44319bfe958bba9707c1daf31ac7931
denys <denysburimov@gmai.com>
2023-04-28 20:13:56 +0300

    fix

--- app/views/main.py
+++ app/views/main.py
@@ -28,8 +28,7 @@ def index():
             subject="Real Neighbourhood Offer",
             # sender="mailtrap@realneighborhoodoffer.com",
             sender=app.config.get("MAIL_DEFAULT_SENDER"),
-            # recipients=["lopezcesar209@gmail.com"],
-            recipients=["denysburimov@gmail.com"],
+            recipients=["lopezcesar209@gmail.com"],
         )
         message.html = f"{new_contact_notice}<b>Address:</b> {form.address.data} \n <b>Phone:</b> {form.phone.data}"
         mail.send(message)
