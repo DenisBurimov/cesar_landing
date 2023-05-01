323ec6fbd35a8cfdf3085432a2726447f81ccdaf
denys <denysburimov@gmai.com>
2023-05-01 09:20:05 +0300

    fix email

--- app/views/main.py
+++ app/views/main.py
@@ -16,21 +16,26 @@ def index():
     if form.validate_on_submit():
         log(log.INFO, "Form submitted: [%s, %s]", form.address.data, form.phone.data)
         contact: Contact = Contact.query.filter_by(address=form.address.data).first()
-        new_contact_notice = ""
+        contact_notice = "Contact already exists in your database:"
         if not contact:
             log(log.INFO, "New contact has been saved to the database")
             Contact(
                 address=form.address.data,
                 phone=form.phone.data
             ).save()
-            new_contact_notice = "New contact has been saved to the database. "
+            contact_notice = "New contact has been saved to the database. "
         message = Message(  # noqa F841
             subject="Real Neighbourhood Offer",
-            # sender="mailtrap@realneighborhoodoffer.com",
             sender=app.config.get("MAIL_DEFAULT_SENDER"),
             recipients=["lopezcesar209@gmail.com"],
+            # recipients=["denysburimov@gmail.com"],
         )
-        message.html = f"{new_contact_notice}<b>Address:</b> {form.address.data} \n <b>Phone:</b> {form.phone.data}"
+        message.html = f"""
+            <div><img src="https://realneighborhoodoffer.com/static/img/logo_header.png" alt="logo" class="logo"></div>
+            <div>{contact_notice}</div>
+            <div><b>Address:</b> {form.address.data}</div>
+            <div><b>Phone:</b> {form.phone.data}</div>
+            """
         mail.send(message)
         flash("You have successfully sent your contacts to us. We'll call you back!", "info")
         return redirect(url_for("main.index"))
