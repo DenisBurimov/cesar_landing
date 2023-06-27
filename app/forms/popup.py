0c63313b984bd7da29a7a49bed63aa8a3298c2a9
AndreyBerezyanskiy <andrey.berezyanskiy@gmail.com>
2023-06-27 11:28:00 +0300

    checkbox to popup was added

--- app/forms/popup.py
+++ app/forms/popup.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, EmailField
+from wtforms import StringField, SubmitField, EmailField, BooleanField
 from wtforms.validators import DataRequired
 
 
 class PopUpForm(FlaskForm):
     address = StringField("Property Address", [DataRequired()])
+    checkbox_terms = BooleanField("Yes", [DataRequired()])
     first_name = StringField("First name")
     last_name = StringField("Last Name")
     email = EmailField("Email")
